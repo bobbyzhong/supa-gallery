a41d1639b9e7c542f5cf561018c28db95deefb66
bobbyzhong <3bobbyzhong3@gmail.com>
2023-03-05 02:16:40 -0800

    connected to supabase and added images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 import { useState } from "react";
 import Image from "next/image";
+import { createClient } from "@supabase/supabase-js";
 
-export default function Gallery() {
+export async function getStaticProps() {
+    const supabaseAdmin = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+        process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+    );
+
+    const { data } = await supabaseAdmin.from("images").select("*").order("id");
+
+    return {
+        props: {
+            images: data,
+        },
+    };
+}
+
+type Image = {
+    id: number;
+    href: string;
+    imageSrc: string;
+    name: string;
+    username: string;
+};
+
+export default function Gallery({ images }: { images: Image[] }) {
     return (
         <div
             className="max-w-2xl mx-auto py-16 px-4 sm:py-24
@@ -11,24 +35,26 @@ export default function Gallery() {
                 className="grid grid-cols-1 gap-y-10 sm:grid-cols-2
         gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
             >
-                <BlurImage />
+                {images.map((image) => (
+                    <BlurImage key={image.id} image={image} />
+                ))}
             </div>
         </div>
     );
 }
 
-function BlurImage() {
+function BlurImage({ image }: { image: Image }) {
     const [isLoading, setLoading] = useState(true);
 
     return (
-        <a href="#" className="group">
+        <a href={image.href} className="group">
             <div
                 className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full
     overflow-hidden rounded-lg bg-gray-200"
             >
                 <Image
                     alt=""
-                    src="https://bit.ly/placeholder-img"
+                    src={image.imageSrc}
                     fill
                     style={{ objectFit: "cover" }}
                     className={`group-hover:opacity-75 duration-700 ease-in-out
@@ -40,8 +66,10 @@ function BlurImage() {
                     onLoadingComplete={() => setLoading(false)}
                 />
             </div>
-            <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-            <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+            <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+            <p className="mt-1 text-lg font-medium text-gray-900">
+                {image.username}
+            </p>
         </a>
     );
 }
